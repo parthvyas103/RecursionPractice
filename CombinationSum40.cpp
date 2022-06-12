f83ad328313534c97d74fc6c227ed9f58bb49312
parthvyas103 <46166077+parthvyas103@users.noreply.github.com>
2022-06-12 13:28:13 +0530

    Update CombinationSum40.cpp

--- CombinationSum40.cpp
+++ CombinationSum40.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(vector<vector<int>> &res, vector<int> curr, vector<int> &candidates, int target, int cursum, int i){
+    void solve(vector<vector<int>> &res, vector<int> curr, int target, vector<int> &candidates, int i){
         if(target == 0){
             res.push_back(curr);
             return;
@@ -8,18 +8,17 @@ public:
         if(target < 0) return;
         if(i == candidates.size())
             return;
-        
-        solve(res, curr, candidates, target, cursum, i+1);
+        solve(res, curr, target, candidates, i+1);
         curr.push_back(candidates[i]);
-        cursum += candidates[i];
-        solve(res, curr, candidates, target, cursum, i+1);
+        solve(res, curr, target-candidates[i],candidates, i);
+        curr.pop_back();
         
     }
     
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> res;
         vector<int> curr;
-        solve(res, curr, candidates, target, 0, 0);
+        solve(res, curr, target, candidates, 0);
         return res;
     }
 };
